5c1e9581070997d0f8b52780495cc8491b44eee3
luhao <luhao.liang@nufront.com>
2020-10-14 17:08:15 +0800

    .

--- views/tasks/task.py
+++ views/tasks/task.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import redis
 from settings import MQTT_HOST, MQTT_PORT, MQTT_PASSWORD, MQTT_USERNAME, REDIS_HOST, REDIS_PORT, LOCAL_HOST, LOCAL_PORT
 from paho.mqtt import client as mqtt_client
@@ -8,10 +9,11 @@ from views.tasks.offline_logfile_rsync import rsync_remote_log
 job_list = [
     {
         "func": "clean_line",
-        "trigger": "cron",
-        "day_of_week": "*",
-        "hour": "22",
-        # "minute": "03",
+        "trigger": "interval",
+        # "trigger": "cron",
+        # "day_of_week": "*",
+        # "hour": "22",
+        "minutes": 60,
     },
 
     # {
@@ -31,7 +33,7 @@ def clean_line():
         "url": "http://{}:{}/log/clean".format(LOCAL_HOST, LOCAL_PORT)
     }
     payload_str = json.dumps(payload)
-    print("task public topic: clean_offset, payload:{}".format(payload_str))
+    logging.debug("task public topic: clean_offset, payload:{}".format(payload_str))
     client.publish("/5476752146/clean_offset", payload_str, 1)
 
 
@@ -43,10 +45,10 @@ def add_job(scheduler):
 
 
 def core():
-    print("delay task start ")
+    logging.debug("delay task start ")
     scheduler = BackgroundScheduler()
     add_job(scheduler)
-    print("started delay task", scheduler.get_jobs())
+    logging.debug("started delay task: %s" % scheduler.get_jobs())
     scheduler.start()
 
 
